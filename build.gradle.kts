53f2ce86ad45fc68aa43272045337c9c32ae74ef
ray123bro-cmd <ray123bro@gmail.com>
2026-08-26 00:28:03 +0300

    Configure Android application with basic settings
    Updated Android plugin configuration and added basic Android setup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
+
+android {
+    namespace = "com.mobileparts.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mobileparts.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
 }
