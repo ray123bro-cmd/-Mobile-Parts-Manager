0a7389d39a45318bc1348f05a5d3accea6977838
ray123bro-cmd <ray123bro@gmail.com>
2026-08-25 16:00:05 +0300

    Configure Android app settings in build.gradle.kts
    Updated Android and Kotlin plugin configurations, set compile and target SDK versions, and defined default app configurations.

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,4 +1,36 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mobileparts.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mobileparts.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
